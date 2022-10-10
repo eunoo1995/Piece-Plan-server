c8fa629d1bfa6c5ca7b1eb8ba42bae9c4edb8c3f
eunoo1995 <eunoo1995@gmail.com>
2022-10-10 20:44:38 +0900

    feat: server error fix test
    서버 동작 이상 현상 수정 코드 작성

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const app = express();
 const PORT = process.env.PORT ? process.env.PORT : 8090;
 
 // cors 해결을 위한 미들웨어 사용
-const ALLOW_LIST = ["https://piece-plan-server.herokuapp.com", "http://localhost:5500"];
+// const ALLOW_LIST = ["http://localhost:5500"];
 
-const corsOptions = {
-  origin(origin, callback) {
-    if (ALLOW_LIST.includes(origin)) callback(null, true);
-    else callback(new Error("Not Allowed Origin!"));
-  },
-};
+// const corsOptions = {
+//   origin(origin, callback) {
+//     if (ALLOW_LIST.includes(origin)) callback(null, true);
+//     else callback(new Error("Not Allowed Origin!"));
+//   },
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
-let todos = [{ a: 1 }, { b: 2 }];
+let todos = [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }];
 
 // REST API
 // callback에 인수로 req와 res가 온다.
